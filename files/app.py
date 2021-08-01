cbbe3e6ef90e1b72635224d0b77076a43dbfe2e1
swish-ds <swish.ds@gmail.com>
2021-08-01 21:50:53 +1000

    edited app.py

--- files/app.py
+++ files/app.py
@@ -23,7 +23,7 @@ def hello():
     # app.logger.error("I'm a ERROR message")
     # app.logger.info("I'm an INFO message")
     count = get_hit_count()
-    return 'Hello World! :) I have been seen {} times.\n'.format(count)
+    return 'Hello World! I have been seen {} times.\n'.format(count)
 
 if __name__ != '__main__':
     gunicorn_logger = logging.getLogger('gunicorn.error')
